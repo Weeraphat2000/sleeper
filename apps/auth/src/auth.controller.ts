aa764c8958d50c66adc70dee7cb283f20d12e009
Weeraphat2000 <weeraphat.main@gmail.com>
2025-01-22 10:02:25 +0700

    update config, docker compose

--- apps/auth/src/auth.controller.ts
+++ apps/auth/src/auth.controller.ts
@@ -1,12 +1,18 @@
 import { Controller, Get } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { ConfigService } from '@nestjs/config';
+import { log } from 'console';
 
 @Controller()
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly configService: ConfigService,
+  ) {}
 
   @Get()
   getHello(): string {
+    log(`ASSA: ${this.configService.get<string>('ASSA')}`); // จะดึงค่ามาจาก environment(.env) variable ที่ชื่อว่า ASSA
     return this.authService.getHello();
   }
 }
--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -3,9 +3,34 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from './users/users.module';
 import { LoggerModule } from '@app/common/logger/logger.module';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [UsersModule, LoggerModule],
+  imports: [
+    UsersModule,
+    LoggerModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(), // configModule จะต้องมี MONGODB_URI
+        JWT_SECRET: Joi.string().required(), // configModule จะต้องมี JWT_SECRET
+        JWT_EXPIRATION: Joi.string().required(), // configModule จะต้องมี JWT_EXPIRATION
+        PORT: Joi.number().required(), // configModule จะต้องมี PORT
+        // PORT: Joi.number().default(3001), // configModule จะมี PORT และ default ค่าเป็น 3001
+      }),
+    }),
+    JwtModule.registerAsync({
+      inject: [ConfigService], // ต้อง inject ConfigService เข้ามาด้วย
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'), // ใช้ JWT_SECRET จาก configModule
+        signOptions: {
+          expiresIn: configService.get<string>('JWT_EXPIRATION'), // ใช้ JWT_EXPIRATION จาก configModule
+        },
+      }),
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
